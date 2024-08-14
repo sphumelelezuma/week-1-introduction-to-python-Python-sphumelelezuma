8d1ce680ea634d4851f5dc1359cb84d0e5fc08bf
sphumelelezuma <zumasphumelele5@gmail.com>
2024-08-14 17:44:03 +0200

    weekly assignment submission

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name here: ") 
+age = input("Enter your age here: ")
+location = input("Enter your location here: ")
+
+message = f"Hi there, my name is {name}, I am {age} years old, I come from {location}."
+print(message)
